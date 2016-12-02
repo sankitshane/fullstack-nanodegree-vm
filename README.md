b4df617d297ea5d87b53debfda89a9af2ca26f56
sankit acharya <sankit.acharya@gmail.com>
2016-12-03 03:56:48 +0530

    comments added

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 rdb-fullstack
 =============
 
-Common code for the Relational Databases and Full Stack Fundamentals courses
+## Description:
+
+It is a application that uses backend database to execute a Swiss tournament matchup
+
+### Software Used:
+* vagrant
+* Virtual Box 4.12.0
+
+### Setup
+
+1. Fort the directory.
+2. Clone the directory to your system.
+3. Install the above Software
+4. On Git Bash go to the directory and then to vagrant directory
+5. Execute vagrant up
+6. Execute vagrant ssh
+7. Go to the root the "cd vagrant"
+
+You will find all the files there.
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,22 +6,28 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Load the file in vagrant psql ny \i tournament.sql
 
+-- Create a database tournament
 CREATE DATABASE tournament;
 
+-- connect to the database tournament by \c tournament
 \c tournament;
 
+--Create table players
 CREATE TABLE players (
       Id SERIAL PRIMARY KEY NOT NULL,
       Name VARCHAR(20) NOT NULL
 );
 
+-- Create table results
 CREATE TABLE results (
       Id INT references players(Id),
       Wins INT,
       Matches INT
 );
 
+--Create table rounds
 CREATE TABLE rounds (
       Player1 INT NOT NULL,
       Player2 INT NOT NULL,
